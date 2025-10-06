dd92b09b4a15e84bd4944c8148eaf213ca4d4068
Ghada <ghada@MacBook-Pro-de-Ghada.local>
2025-10-06 11:52:19 +0100

    update number of search results

--- search/web_search.py
+++ search/web_search.py
@@ -2,7 +2,7 @@ from langchain_community.tools import DuckDuckGoSearchRun
 from models.SearchAssistantState import SearchState
 
 
-duck_tool = DuckDuckGoSearchRun(top_k=1)
+duck_tool = DuckDuckGoSearchRun(top_k=2)
 
 def web_search(state: SearchState):
     result = duck_tool.run(state.query)
--- search/wikipedia_search.py
+++ search/wikipedia_search.py
@@ -1,7 +1,7 @@
 from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
 from models.SearchAssistantState import SearchState
 
-wiki_tool = WikipediaAPIWrapper(top_k_results=1)
+wiki_tool = WikipediaAPIWrapper(top_k_results=2)
 
 def wikipedia_search(state: SearchState):
     result = wiki_tool.run(state.query)
